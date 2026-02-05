5f7a1925c77de7d75e76a24b1e2e9187bacfa65e
StartAutomating <james.brundage@start-automating.com>
2026-02-05 13:04:06 -0800

    feat: Escape.Sequence.proportionalSpacing ( Fixes #26 )

---
+++ Types/Escape.Sequence/proportionalSpacing.ps1
@@ -0,0 +1,16 @@
+<#
+.SYNOPSIS
+    Proportial Spacing Escape Sequence
+.DESCRIPTION
+    Escape Sequence to stop Proportial Spacing
+.NOTES
+    Unused in modern terminals
+.LINK
+    https://en.wikipedia.org/wiki/ANSI_escape_code
+#>
+param()
+
+$e = [char]27
+"$e[26m"
+
+
