d55b117393121b911aeaa5e256cf05bec2f5c2c6
StartAutomating <james.brundage@start-automating.com>
2026-02-06 13:30:58 -0800

    feat: Escape.Sequence.italic ( Fixes #66 )

---
+++ Types/Escape.Sequence/italic.ps1
@@ -0,0 +1,12 @@
+<#
+.SYNOPSIS
+    Italic Escape Sequence
+.DESCRIPTION
+    Escape Sequence for italics 
+.LINK
+    https://en.wikipedia.org/wiki/ANSI_escape_code
+#>
+param()
+
+$e = [char]27
+"$e[3m"
