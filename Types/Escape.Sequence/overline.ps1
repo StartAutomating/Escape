1327e00f556e9c26cf2d6b1df5b27d6cd4ee3db0
StartAutomating <james.brundage@start-automating.com>
2026-02-05 23:28:12 -0800

    feat: Escape.Sequence.overline ( Fixes #53 )

---
+++ Types/Escape.Sequence/overline.ps1
@@ -0,0 +1,12 @@
+<#
+.SYNOPSIS
+    Overline Escape Sequence
+.DESCRIPTION
+    Escape Sequence for overline
+.LINK
+    https://en.wikipedia.org/wiki/ANSI_escape_code
+#>
+param()
+
+$e = [char]27
+"$e[53m"
