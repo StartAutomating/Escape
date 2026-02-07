9aaee35e421db0c46263e0721e6d5d16eabf6b2b
StartAutomating <james.brundage@start-automating.com>
2026-02-06 21:19:10 -0800

    feat: Escape.Sequence.backgroundBright ( Fixes #100, Fixes #101, Fixes #102, Fixes #103, Fixes #104, Fixes #105, Fixes #106, Fixes #107 )

---
+++ Types/Escape.Sequence/backgroundBrightBlue.ps1
@@ -0,0 +1,12 @@
+<#
+.SYNOPSIS
+    Background Bright Blue Escape Sequence
+.DESCRIPTION
+    Escape Sequence to set Background to Bright Blue
+.NOTES
+    This color is xterm color number 12
+.LINK
+    https://en.wikipedia.org/wiki/ANSI_escape_code
+#>
+$e = [char]27
+"$e[104m"
---
+++ Types/Escape.Sequence/backgroundBrightPurple.ps1
@@ -0,0 +1,12 @@
+<#
+.SYNOPSIS
+    Background Bright Purple Escape Sequence
+.DESCRIPTION
+    Escape Sequence to set Background to Bright Purple
+.NOTES
+    This color is xterm color number 13
+.LINK
+    https://en.wikipedia.org/wiki/ANSI_escape_code
+#>
+$e = [char]27
+"$e[105m"
