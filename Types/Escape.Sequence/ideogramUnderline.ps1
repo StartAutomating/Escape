ed3d1fdef649dea3f3b8651e51550b5d55a91857
StartAutomating <james.brundage@start-automating.com>
2026-02-10 17:06:24 -0800

    feat: Escape.Sequence.ideogramUnderline ( Fixes #60 )

---
+++ Types/Escape.Sequence/ideogramUnderline.ps1
@@ -0,0 +1,12 @@
+<#
+.SYNOPSIS
+    Ideogram Underline Escape Sequence
+.DESCRIPTION
+    Escape Sequence for Ideogram Underline
+    
+    Ideogram escape sequences are supported in some loclized terminals.
+#>
+param()
+$e = [char]27
+"$e[60m"
+
