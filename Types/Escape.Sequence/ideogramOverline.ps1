658089cbe17002f2fae7c500949372bb777544e8
StartAutomating <james.brundage@start-automating.com>
2026-02-10 17:04:57 -0800

    feat: Escape.Sequence.ideogramOverline ( Fixes #62 )

---
+++ Types/Escape.Sequence/ideogramOverline.ps1
@@ -0,0 +1,12 @@
+<#
+.SYNOPSIS
+    Ideogram Overline Escape Sequence
+.DESCRIPTION
+    Escape Sequence for Ideogram Overline
+    
+    Ideogram escape sequences are supported in some loclized terminals.
+#>
+param()
+$e = [char]27
+"$e[62m"
+
