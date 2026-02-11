32554b0dc2225296a2228c5c40c405f8826f5a3d
StartAutomating <james.brundage@start-automating.com>
2026-02-10 17:00:54 -0800

    feat: Escape.Sequence.ideogramStress ( Fixes #64 )

---
+++ Types/Escape.Sequence/ideogramStress.ps1
@@ -0,0 +1,12 @@
+<#
+.SYNOPSIS
+    Ideogram Stress Escape Sequence
+.DESCRIPTION
+    Escape Sequence for Ideogram Stress
+    
+    Ideogram escape sequences are supported in some loclized terminals.
+#>
+param()
+$e = [char]27
+"$e[64m"
+
